728c0382564b8c406041c1651d4fbe0b1f6c5002
Joaquin Sosa <129340097+JoaquinSosa2607@users.noreply.github.com>
2023-09-25 19:26:49 -0300

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,26 @@ app.get('/', (req,res) => {
     })
 })
 
+app.get('/products', async (req,res) => {
+    try{
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.get('/products/:id', async (req,res) => {
+    try{
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+
 app.post('/product', async (req,res) => {
     try{
 
@@ -23,6 +43,23 @@ app.post('/product', async (req,res) => {
         res.status(200).json(product);
 
     } catch{
+        console.log(error.message);
+        res.status(500).json({message: error.message})
+    }
+})
+
+ // Update a Product
+app.put('/products/:id', async (req,res) => {
+
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID: ${id}`})
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
         console.log(error.message);
         res.status(500).json({message: error.message})
     }
